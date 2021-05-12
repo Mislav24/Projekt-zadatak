ea12a1ee9e2e18b6a2c80b05dfd9caa16a68d466
Mislav Simic <mislav49992@gmail.com>
2021-05-12 22:33:16 +0200

    adding content

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -11,14 +11,47 @@ export class FormUserDetails extends Component {
     }
 
     render() {
-        const { values } = this.props;
+        const { values, handleChange } = this.props;
         return (
             <MuiThemeProvider>
                 <React.Fragment>
                     <AppBar title="Enter User Details" />
+                    <TextField 
+                    hintText="Enter your First Name"
+                    floatingLabelText="First Name"
+                    onChange={handleChange('firstName')}
+                    defaultValue={values.firstName}
+                    />
+                    <br/>
+                    <TextField 
+                    hintText="Enter your Last Name"
+                    floatingLabelText="Last Name"
+                    onChange={handleChange('lastName')}
+                    defaultValue={values.lastName}
+                    />
+                    <br/>
+                    <TextField 
+                    hintText="Enter your Email"
+                    floatingLabelText="Email"
+                    onChange={handleChange('email')}
+                    defaultValue={values.email}
+                    />
+                    <br/>
+                    <RaisedButton 
+                        label="Continue"
+                        primary={true}
+                        style={styles.button}
+                        onClick={this.continue}
+                    />
                 </React.Fragment>
             </MuiThemeProvider>
         );
+    }
+}
+
+const styles = {
+    button:{
+        margin: 15
     }
 }
 
