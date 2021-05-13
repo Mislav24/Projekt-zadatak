17cbca91c178986ff5dde2a2bc99f1b2ce68db70
Mislav Simic <mislav49992@gmail.com>
2021-05-13 23:16:41 +0200

    Adding PersonalDeatils and Confirm

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import FormUserDetails from './FormUserDetails'
+import FormPersonalDetails from './FormPersonalDetails'
+import Confirm from './Confirm'
 
 export class UserForm extends Component {
+    
+
     state = {
         step: 1,
         firstName:'',
@@ -43,11 +47,25 @@ export class UserForm extends Component {
                         handleChange={this.handleChange}
                         values={values}
                     />
-                )
+                );
                 case 2:
-                    return <h1>FormPersonalDetails</h1>
+                    return(
+                        <FormPersonalDetails 
+                            nextStep={this.nextStep}
+                            prevStep={this.prevStep}
+                            handleChange={this.handleChange}
+                            values={values}
+                        />
+                    );
                 case 3:
-                    return <h1>Cofirm</h1>
+                    return(
+                        <Confirm 
+                            nextStep={this.nextStep}
+                            prevStep={this.prevStep}
+                            handleChange={this.handleChange}
+                            values={values}
+                        />
+                    );
                 case 4:
                     return <h1>Success</h1>
         }
